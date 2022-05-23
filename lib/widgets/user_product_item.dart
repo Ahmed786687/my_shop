6907dcd7df123dd6fcb3e64bec5d7872533c6458
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-05-24 01:10:49 +0500

    Here is an ListView Error near Trailing widgets

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class UserProductItem extends StatelessWidget {
+  const UserProductItem({Key? key, required this.title, required this.imageUrl})
+      : super(key: key);
+
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: ConstrainedBox(
+        constraints: const BoxConstraints(minHeight: 100, minWidth: 100),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.edit,
+                size: 12,
+              ),
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                size: 12,
+              ),
+              color: Theme.of(context).errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
